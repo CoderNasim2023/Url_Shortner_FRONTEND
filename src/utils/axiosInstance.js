4126b3efb039709c0e560b94129ec275e1cc7539
Md Nasim Akhtar <Nasimjam89@gmail.com>
2025-12-23 20:20:56 +0530

    Changed  BACKEND RENDER URL LINK

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000", // Use relative URLs so Vite dev proxy forwards /api to backend (cookies become first-party)
+    baseURL: "https://url-shortner-backend-qoag.onrender.com", // Use relative URLs so Vite dev proxy forwards /api to backend (cookies become first-party)
     timeout: 30000, //10s
     withCredentials: true
 })
